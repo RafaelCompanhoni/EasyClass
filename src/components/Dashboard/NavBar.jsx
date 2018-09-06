8026c65f131757053c182ee2b7edc0aa7129b326
Rafael Companhoni <companhoni@gmail.com>
2018-09-05 22:32:29 -0300

    Adicionado estado para controlar menu burger

--- src/components/Dashboard/NavBar.jsx
+++ src/components/Dashboard/NavBar.jsx
@@ -1,54 +1,71 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 import CSSModules from 'react-css-modules';
 import styles from '../../../styles/custom/Dashboard/navbar.sass';
 
-const NavBar = ({ onLogout }) => (
-  <nav
-    styleName="navbar is-dark"
-    role="navigation"
-    aria-label="main navigation"
-  >
-    <div styleName="navbar-brand">
-      <a styleName="navbar-item" href="https://bulma.io">
-        <img
-          src="https://bulma.io/images/bulma-logo-white.png"
-          alt="Bulma: a modern CSS framework based on Flexbox"
-          width="112"
-          height="28"
-        />
-      </a>
+class NavBar extends Component {
+  constructor(props) {
+    super(props);
 
-      <a
-        role="button"
-        styleName="navbar-burger"
-        aria-label="menu"
-        aria-expanded="false"
+    this.state = {
+      navBarExpanded: false,
+    };
+  }
+
+  render() {
+    const { onLogout } = this.props;
+
+    return (
+      <nav
+        styleName="navbar is-dark"
+        role="navigation"
+        aria-label="main navigation"
       >
-        <span aria-hidden="true" />
-        <span aria-hidden="true" />
-        <span aria-hidden="true" />
-      </a>
-    </div>
+        <div styleName="navbar-brand">
+          <a styleName="navbar-item" href="https://bulma.io">
+            <img
+              src="https://bulma.io/images/bulma-logo-white.png"
+              alt="Bulma: a modern CSS framework based on Flexbox"
+              width="112"
+              height="28"
+            />
+          </a>
 
-    <div styleName="navbar-end">
-      <div styleName="navbar-item">
-        <div styleName="field is-grouped">
-          <p styleName="control">
-            <a styleName="button is-primary" onClick={onLogout}>
-              <span styleName="icon">
-                <FontAwesomeIcon icon={faSignOutAlt} />
-              </span>
-              <span>Logout</span>
-            </a>
-          </p>
+          <a
+            role="button"
+            styleName="navbar-burger"
+            aria-label="menu"
+            aria-expanded="false"
+            onClick={() => this.setState({ navBarExpanded: !this.state.navBarExpanded })}
+          >
+            <span aria-hidden="true" />
+            <span aria-hidden="true" />
+            <span aria-hidden="true" />
+          </a>
         </div>
-      </div>
-    </div>
-  </nav>
-);
+
+        <div className={`navbar-menu ${this.state.navBarExpanded ? 'is-active' : ''}`}>
+          <div styleName="navbar-end">
+            <div styleName="navbar-item">
+              <div styleName="field is-grouped">
+                <p styleName="control">
+                  <a styleName="button is-primary" onClick={onLogout}>
+                    <span styleName="icon">
+                      <FontAwesomeIcon icon={faSignOutAlt} />
+                    </span>
+                    <span>Logout</span>
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
 
 NavBar.propTypes = {
   onLogout: PropTypes.func.isRequired,
